917a0748db100e3de368a543de271b3df4001331
dghollingsworth <dghollingsworth@gmail.com>
2014-03-27 13:20:14 -0400

    people.rb

--- people.rb
+++ people.rb
@@ -1,19 +1,39 @@
-class Student
+class Person
+  def initialize(name)
+    @name = name
+  end
+  
+  def greeting
+  	"Hi my name is, #{@name}"
+  end
+
+  def to_s
+  	greeting
+  end
+end
+
+class Student < Person
   def learn 
   	"I get it"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
   	"Everything is Ruby is an Object"
   end
 end
 
-class Person
-  
-  initialize(name)
-    @name = name
-  end
-end
+chris = Instructor.new("Chris")
+cristina = Student.new("Cristina")
+
+puts chris
+puts cristina
+puts chris.teach
+puts cristina.learn
+puts chris.learn
+puts cristina.teach
+
+#It doesn't work because the student and teach don't have access to eachothers information
+#they only have access to what's in their own and their parent's class
 
